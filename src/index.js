2ae4703471ed2599c7489af565dde86ef3b9c55b
Benjamin Cheng <bencheng200@gmail.com>
2021-05-13 02:00:55 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector('form');
 
+  form.addEventListener('submit', function(e)){
+    e.preventDefault();
+  }
   
 });
